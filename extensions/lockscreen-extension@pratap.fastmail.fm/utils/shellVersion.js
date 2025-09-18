fce4ed2871eba5f54c2ddb7d51ed6c71542aa13b
tucab <turtlecabbage@protonmail.com>
2025-09-18 15:20:29 -0400

    as i move away from gnome these extensions will not be used

--- extensions/lockscreen-extension@pratap.fastmail.fm/utils/shellVersion.js
+++
@@ -1,5 +0,0 @@
-import * as Config from 'resource:///org/gnome/shell/misc/config.js';
-
-const GNOME_SHELL_VERSION = Number(Config.PACKAGE_VERSION.split('.')[0]);
-
-export default GNOME_SHELL_VERSION;
